7964f7268402d134edf3100245493092ad38981e
Amr Emam <99700574+amrremam@users.noreply.github.com>
2024-05-20 02:14:56 +0300

    Trie.go

---
+++ Trie.go
@@ -0,0 +1,54 @@
+package main
+
+import "fmt"
+
+const AlphabetSize = 26
+
+type Node struct {
+	children [AlphabetSize]*Node
+	isEnd    bool
+}
+
+type Trie struct {
+	root *Node
+}
+
+func InitTrie() *Trie {
+	result := &Trie{root: &Node{}}
+	return result
+}
+
+func (t *Trie) Insert(w string) {
+	wordLength := len(w)
+	currentNode := t.root
+	for i := 0; i < wordLength; i++ {
+		charIndex := w[i] - 'a'
+		if currentNode.children[charIndex] == nil {
+			currentNode.children[charIndex] = &Node{}
+		}
+		currentNode = currentNode.children[charIndex]
+	}
+	currentNode.isEnd = true
+}
+
+func (t *Trie) search(w string) bool {
+	wordLength := len(w)
+	currentNode := t.root
+	for i := 0; i < wordLength; i++ {
+		charIndex := w[i] - 'a'
+		if currentNode.children[charIndex] == nil {
+			return false
+		}
+		currentNode = currentNode.children[charIndex]
+	}
+	if currentNode.isEnd == true {
+		return true
+	}
+	return false
+}
+
+func main() {
+	myTrie := InitTrie()
+	myTrie.Insert("amr")
+	fmt.Println(myTrie.search("amr"))
+}
